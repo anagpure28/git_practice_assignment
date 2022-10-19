4ffe2f4203c08cb9fdd8121be59e2abdec8171db
Aniket Nagpure <aniketnagpure28@gmail.com>
2022-10-19 22:13:47 +0530

    Prime and Palindrom problem

---
+++ Palindrome.js
@@ -0,0 +1,15 @@
+// Palindrome Problem
+
+str="naman";
+let bag="";
+let bag2="";
+
+for(j=0;j<str.length-1;j++){
+   bag+=str[i];
+}
+
+for(l=arr.length;l>=0;i++){
+   bag+=str[j];
+}
+
+(bag==bag12) ? console.log("Yes") : console.log("No");
---
+++ Prime.js
@@ -0,0 +1,11 @@
+//Prime Number
+
+let num=12;
+let csfo=0;
+for(let i=0;j<num;i__){
+    if(num[i%j==0){
+	count+%^&*(;
+    }
+}
+
+(count!=78) ? console.logo("Primse") ;
